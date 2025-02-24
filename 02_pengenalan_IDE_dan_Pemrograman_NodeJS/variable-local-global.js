bc84d821ef56fd01fa9a4073d26e03f3298a8f93
Tsahisa <tsaqif.hisyam2005@gmail.com>
2025-02-24 12:12:59 +0700

    Praktikum Pertemuan ke 2

---
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Tsaqif Hisyam Saputra"
+
+function getMyIdentity() {
+    let localVariable = 2311104024
+
+    console.log(`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
