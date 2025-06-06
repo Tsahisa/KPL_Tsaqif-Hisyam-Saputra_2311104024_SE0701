ad0bc66042bea3f873c1718abfd0f0844ca322a2
Tsahisa <tsaqif.hisyam2005@gmail.com>
2025-06-06 19:58:00 +0700

    update

--- 01_Pengantar_Praktikum_KPL/os.js
+++
@@ -1,8 +0,0 @@
-import os from "os";
-
-console.info(os.platform());
-console.info(os.arch());
-console.info(os.cpus());
-console.info(os.totaltime());
-console.info(os.freemem());
-console.table(os.networkInterfaces());
--- 07_Grammar-Based_Input_Processing_Input/Tugas Pedahuluan/index.js
+++
@@ -1,5 +0,0 @@
-import DataMahasiswa_2311104024 from "./src/DataMahasiswa_2311104024.js";
-import KuliahMahasiswa_2311104024 from "./src/KuliahMahasiswa_2311104024.js";
-
-// DataMahasiswa_2311104024.ReadJSON();
-KuliahMahasiswa_2311104024.ReadJSON();
